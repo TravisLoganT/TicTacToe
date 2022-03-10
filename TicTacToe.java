c38b7fb069e0255a43b7229a71e7de166280e8cc
Travis Logan <c1014427@newcastle.ac.uk>
2022-03-10 23:11:17 +0000

    method to place a symbol on the game-board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    public static final Scanner consoleInput = new Scanner(System.in);
 
 
     public static void main(String[] args) {
@@ -10,6 +14,13 @@ public class TicTacToe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
 
+        printGameBoard(gameBoard);
+
+
+        System.out.println("Enter where you would like to play (1-9)");
+        int chosenPosition = consoleInput.nextInt();
+
+        placeSymbol(gameBoard, chosenPosition, "CPU");
         printGameBoard(gameBoard);
 
     }
@@ -27,6 +38,28 @@ public class TicTacToe {
         }
 
 
+    }
+
+    public static void placeSymbol(char[][] gameBoard, int position, String user){
+
+        char symbol = ' ';
+        if (user.equals("Player")){
+            symbol = 'X';
+        } else if (user.equals("CPU")){
+            symbol = 'O';
+        }
+        switch (position) {
+            case 1 -> gameBoard[0][0] = symbol;
+            case 2 -> gameBoard[0][2] = symbol;
+            case 3 -> gameBoard[0][4] = symbol;
+            case 4 -> gameBoard[2][0] = symbol;
+            case 5 -> gameBoard[2][2] = symbol;
+            case 6 -> gameBoard[2][4] = symbol;
+            case 7 -> gameBoard[4][0] = symbol;
+            case 8 -> gameBoard[4][2] = symbol;
+            case 9 -> gameBoard[4][4] = symbol;
+            default -> throw new IllegalStateException("Unexpected value: " + position);
+        }
     }
 
 
