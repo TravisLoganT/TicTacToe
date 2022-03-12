900b256b0aad23b42091eb1485b2f1e38ab4c591
Travis Logan <c1014427@newcastle.ac.uk>
2022-03-12 09:46:35 +0000

    added the decision to play against the computer

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 
 public class TicTacToe {
@@ -8,6 +10,11 @@ public class TicTacToe {
     public static String result = "";
 
     public static void main(String[] args) {
+        gameChoice();
+
+
+    }
+    public static void playAgainstComputer(){
         // Show the user the GameBoard
         GameBoard.printGameBoard();
 
@@ -22,6 +29,25 @@ public class TicTacToe {
             Winner.checkWinner();
 
             GameBoard.printGameBoard();
+        }
+    }
+
+    public static void gameChoice(){
+
+        System.out.println("What mode would you like to play?\n-------------------------------");
+        System.out.println("1. Against the computer" +
+                "\n2. Against another player");
+        String choice = consoleInput.nextLine();
+
+        while (true) {
+
+            if (choice.equals("1")) {
+                playAgainstComputer();
+            } else if (choice.equals("2")) {
+                // playAgainstPlayer();
+            } else {
+                System.out.println("You have not inputted a correct choice");
+            }
         }
     }
 }
